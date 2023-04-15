5fab12f1bd5c0e8cb54ba1402c5993454374566e
Hilmi Mert ACAR <hmrtacar@hotmail.com>
2023-04-15 20:50:53 +0300

    changed css and delete button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,7 +10,35 @@ const ExpenseItem = (props) => {
         <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
-          <button className="expense-item__delete-button" onClick={() => props.onDelete(props.expenseKey)}>Delete</button>
+          <button
+            className="expense-item__delete-button"
+            onClick={() => props.onDelete(props.expenseKey)}
+          >
+            <svg
+              width={12}
+              height={12}
+              viewPort="0 0 12 12"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line
+                x1="0"
+                y1="12"
+                x2="12"
+                y2="0"
+                stroke="white"
+                stroke-width="3"
+              />
+              <line
+                x1="0"
+                y1="0"
+                x2="12"
+                y2="12"
+                stroke="white"
+                stroke-width="3"
+              />
+            </svg>
+          </button>
         </div>
       </Card>
     </li>
